5eb61c1ce9fea2232cc03f87ddcf8f977c9abfe0
Aakaash Jois <aakaashjois@gmail.com>
2017-01-16 14:12:13 +0530

    Updated icons.

--- app/src/main/java/com/biryanistudio/emptywidget/EmptyWidgetProvider.java
+++ app/src/main/java/com/biryanistudio/emptywidget/EmptyWidgetProvider.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.os.Bundle;
 import android.widget.RemoteViews;
 
 import java.util.Random;
@@ -27,6 +28,47 @@ public class EmptyWidgetProvider extends AppWidgetProvider {
         SharedPreferences.Editor editor = settings.edit();
         editor.putBoolean(ENABLE_COLORS, enableColors);
         editor.apply();
+        addColors(context, enableColors);
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        SharedPreferences settings = context.getSharedPreferences("settings", Context.MODE_PRIVATE);
+        if (settings.getBoolean(ENABLE_COLORS, false)) addColors(context, true);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        SharedPreferences settings = context.getSharedPreferences("settings", Context.MODE_PRIVATE);
+        if (settings.getBoolean(ENABLE_COLORS, false)) addColors(context, true);
+    }
+
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager,
+                                          int appWidgetId, Bundle newOptions) {
+        SharedPreferences settings = context.getSharedPreferences("settings", Context.MODE_PRIVATE);
+        if (settings.getBoolean(ENABLE_COLORS, false)) addColors(context, true);
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        SharedPreferences settings = context.getSharedPreferences("settings", Context.MODE_PRIVATE);
+        if (settings.getBoolean(ENABLE_COLORS, false)) addColors(context, true);
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        SharedPreferences settings = context.getSharedPreferences("settings", Context.MODE_PRIVATE);
+        if (settings.getBoolean(ENABLE_COLORS, false)) addColors(context, true);
+    }
+
+    @Override
+    public void onRestored(Context context, int[] oldWidgetIds, int[] newWidgetIds) {
+        SharedPreferences settings = context.getSharedPreferences("settings", Context.MODE_PRIVATE);
+        if (settings.getBoolean(ENABLE_COLORS, false)) addColors(context, true);
+    }
+
+    private void addColors(Context context, Boolean enableColors) {
         AppWidgetManager manager = AppWidgetManager.getInstance(context);
         int ids[] = manager.getAppWidgetIds(new ComponentName(context,
                 EmptyWidgetProvider.class));
