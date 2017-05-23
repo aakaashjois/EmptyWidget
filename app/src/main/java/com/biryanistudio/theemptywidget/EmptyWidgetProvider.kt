0052efdb1a3f0bcc4c917dcbcbee54b734966d6b
Aakaash Jois <aakaashjois@gmail.com>
2017-05-23 15:48:24 +0530

    - Add new launcher icons - Change project package name

--- app/src/main/java/com/biryanistudio/theemptywidget/EmptyWidgetProvider.kt
+++ app/src/main/java/com/biryanistudio/theemptywidget/EmptyWidgetProvider.kt
@@ -1,4 +1,4 @@
-package com.biryanistudio.emptywidget
+package com.biryanistudio.theemptywidget
 
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
--- app/src/main/java/com/biryanistudio/theemptywidget/EnableColorsActivity.kt
+++ app/src/main/java/com/biryanistudio/theemptywidget/EnableColorsActivity.kt
@@ -1,13 +1,13 @@
-package com.biryanistudio.emptywidget
+package com.biryanistudio.theemptywidget
 
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Gravity
 import android.widget.CheckBox
 import org.jetbrains.anko.checkBox
 import org.jetbrains.anko.frameLayout
+import org.jetbrains.anko.intentFor
 import org.jetbrains.anko.sdk25.coroutines.onCheckedChange
 import org.jetbrains.anko.wrapContent
 
@@ -18,13 +18,12 @@ class EnableColorsActivity : AppCompatActivity() {
         var enableCheckBox: CheckBox? = null
         frameLayout {
             enableCheckBox = checkBox {
-                text = "Enable Colors for all widgets"
+                text = "Color all the empty widgets"
                 textSize = 22f
                 gravity = Gravity.CENTER
                 onCheckedChange { _, isChecked ->
-                    val intent = Intent(applicationContext, EmptyWidgetProvider::class.java)
+                    val intent = intentFor<EmptyWidgetProvider>(EmptyWidgetProvider.ENABLE_COLORS to isChecked)
                     intent.action = "android.appwidget.action.APPWIDGET_UPDATE"
-                    intent.putExtra(EmptyWidgetProvider.ENABLE_COLORS, isChecked)
                     sendBroadcast(intent)
                 }
             }.lparams(width = wrapContent, height = wrapContent, gravity = Gravity.CENTER)
